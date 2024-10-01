6231e9e09aae6fafc101916722ba966e47d5ca34
George Papamichail <152964062+gi0rg0sPapamichail@users.noreply.github.com>
2024-10-01 13:41:17 +0300

    Update esp_now_HR_enums.h

--- src/esp_now_HR_enums.h
+++ src/esp_now_HR_enums.h
@@ -26,7 +26,7 @@ enum COMMUNICATION {
 enum INITIALIZATION_ERRORS {
     ESP_NOW_INITIALIZATION_ERROR,        ///< [Error] initializing ESP-NOW                              *1              
     ESP_NOW_COMMUNICATION_SETUP_ERROR,   ///< [Error] in the consructors communication parameter        *2                                         
-    CHANNEL_OUT_OF_RANGRE,               ///< [Error] in the value of the channel (channel ranges 0-1)  *3                                  
+    CHANNEL_OUT_OF_RANGRE,               ///< [Error] in the value of the channel (channel ranges 0-13)  *3                                  
     NEW_MAC_INITIALIZATION_ERROR,        ///< [Error] setting new MAC address                           *4                  
     ADD_PEER_INITIALIZATION_ERROR,       ///< [Error] adding peer                                       *5      
     MEMORY_ALLOCATION_ERROR              ///< [Error] allocating memory                                 *6  
